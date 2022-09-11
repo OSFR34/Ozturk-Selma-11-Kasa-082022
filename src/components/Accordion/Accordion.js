5fa1dc916f1079493233992136bf416e350f8517
OSFR34 <selma-ozturk@outlook.com>
2022-09-11 11:58:05 +0200

    retrait des commentaires superflux

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -4,32 +4,24 @@ import './accordion.css'
 class Accordion extends Component {
   render() {
     return (
-      // bootstrap =container: gère automatiquement les marges à droite et à gauches de façon équitablen. 
-      // my-5 : gère la marge des ordonnées (haut bas)- régalage de my-1 à my-5.my-1 est la valeur la plus petite et my-5 est la plus grande. 
+    
       <div className='container my-5'>
-        {/* Pour créer des colones il faut d'abord créer une ligne. */}
+       
         <div className='row'>
-          {/* col-12 : en mode mobile que la largeur de l'affichage doit occupé l'intégralité de l'écran.  */}
-          {/* col-lg-10: pour les Laptop et plus grand écran la largeur de la colonne doit correspondre à 10. */}
-          {/* offset-lg-1 : bascule la largeur à 1 colonne et pour centrage de celle-ci */}
+         
           <div className='col-12 col-lg-10 offset-lg-1'>
             <div className="accordion" id="accordionExample">
              
               <div className="accordion-item">
                 <h2 className="accordion-header" id="headingOne">
-                  {/* button : accordionun açılıp kapanmasını sağlayan butondur. */}
-                  {/* collapsed: signifie que l'accordeon est fermé */}
-                  {/* data-bs-toggle="collapse" gère l'ouverture et la fermeture de l'accordeon.  */}
-                  {/* data-bs-target="#collapseOne", collapseOne ouvre l'accordeon. */}
-                  {/* aria-controls="collapseOne" signifie quel element doit etre controler*/}
+            
                   <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                     Fiabilité
                   </button>
                 </h2>
-                {/* accordion-collapse: precise ce qui doit etre vu au clic.*/}
-                {/* data-bs-parent="#accordionExample" au clic sue l'accordeon, précise quel est le parent qui est lié. */}
+            
                 <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                  {/* accordion-body :règle automatique les margins et les paddings . */}
+                
                   <div className="accordion-body">
                     Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
                   </div>
