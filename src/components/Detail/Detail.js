a0dbd62196e3edd065eeee80bdf94ff0df0504f1
OSFR34 <selma-ozturk@outlook.com>
2022-09-12 15:34:09 +0200

    renvoi page404 après modification de l'id  lodging

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,46 +1,53 @@
-// création d'un composant fonction pour récupérer id à partir du "path"de l'annonce.
+import React, {useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Slider from "../Slider/Slider";
+import ListCards from "../../data.json";
+import TitleAddress from "../TitleAddress/TitleAddress";
+import HostInfo from "../HostInfo/HostInfo";
+import DetailAccordions from "../DetailAccordions/DetailAccordions";
+import Page404 from "../Page404/Page404";
 
-import React from 'react'
-// utilisation de la méthode useParams() pour récupérer du chemin 'Path'son id.
-import {useParams} from 'react-router-dom';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Slider from '../Slider/Slider';
-import ListCards from '../../data.json';
-import TitleAddress from '../TitleAddress/TitleAddress';
-import HostInfo from '../HostInfo/HostInfo';
-import DetailAccordions from '../DetailAccordions/DetailAccordions';
+export default function Detail() {
+  const parameters = useParams();
+  const [card, setCard] = useState(null);
 
+  useEffect(() => {
+    ListCards.map((lodging) => {
+      if (lodging.id === parameters.lodgingid) {
+        setCard(lodging);
+      }
+    });
+  });
 
-function Detail() {
-  const parameters = useParams(); 
   return (
     <div>
-        <Header/>
-          {/* map :permet l'itération à partir des objets */}
-          {   
-            ListCards.map((card,i) => {
-              if(card.id === parameters.lodgingid){
-                return(
-                
-                  <div key={i}>
-                    <Slider key={i} cardinfo={card} />
-                    <div className='container my-4'>
-                    <div className="row">
-                      <TitleAddress key={i} title={card.title} localisation={card.location} tags={card.tags} />
-                      <HostInfo hostinfo={card.host} rating={card.rating} />
-                      <DetailAccordions equipmentProps = {card.equipments} descriptionProps={card.description} />
-                    </div>
-                    </div>
-                  </div>
-                )
-              }
-             
-            })
-          }
-          
-        <Footer/>
+      {card !== null ? (
+        <>
+          <Header />
+          <div>
+            <Slider cardinfo={card} />
+            <div className="container my-4">
+              <div className="row">
+                <TitleAddress
+                  title={card.title}
+                  localisation={card.location}
+                  tags={card.tags}
+                />
+                <HostInfo hostinfo={card.host} rating={card.rating} />
+                <DetailAccordions
+                  equipmentProps={card.equipments}
+                  descriptionProps={card.description}
+                />
+              </div>
+            </div>
+          </div>
+          <Footer />
+        </>
+      ) : (
+        <Page404 />
+      )}
     </div>
-  )
+  );
 }
-export default Detail;
